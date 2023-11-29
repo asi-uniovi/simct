56193fc2c8d25e673bfa97567ad90ee170c736d2
Joaquín <joaquin@uniovi.es>
2023-11-29 11:06:44 +0100

    Add error handling in main.js
    Exceptions are now catched and an alert is shown to the user

--- main.js
+++ main.js
@@ -7,7 +7,14 @@
 import { Simulator } from './app/view/sim.js'
 import { State } from './app/config/control.js'
 
-const sim = new Simulator()
+try {
+  const sim = new Simulator()
+  window.sim = sim
+  window.state = State
+} catch (e) {
+  console.error(e)
 
-window.sim = sim
-window.state = State
+  // Show an error to user so that he knows that this is probably a bug and not
+  // his fault
+  alert('An unexpected error occurred: ' + e)
+}
