e134ad5756a9d4e05582731a71abdae1b3821ae9
jmolleda <113172525+jmolleda@users.noreply.github.com>
2023-11-30 23:26:35 +0100

    Do not close the signal selector on sending the signals to the control unit

--- app/view/navigation/viewactions.js
+++ app/view/navigation/viewactions.js
@@ -100,7 +100,9 @@ class ViewActions extends Observable {
       document.querySelector('#wmng').append(w.dom.element)
       signalmng.onSave(function () {
         ct.cpu.uc.loadSignals(sim.control.selectedSignals)
-        wm.remove(w)
+        // Do not close the signal selector on sending the signals to the control unit.
+        // The user must click X to close. Uncomment next line to change this behavior.
+		    // wm.remove(w)
       })
     }
   }
